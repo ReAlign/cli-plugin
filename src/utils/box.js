5425fdebdd93a84426e1cf8b9e38db5393929509
ReAlign <realign@yeah.net>
2025-04-28 22:14:30 +0800

    update: 中文长度计算

--- src/utils/box.js
+++ src/utils/box.js
@@ -1,12 +1,22 @@
 const { colors } = require('./color-console');
 
+const getBLen = (str) => {
+  if (str == null) return 0;
+  if (typeof str != 'string') {
+    str += '';
+  }
+  return str.replace(/[^\x00-\xff]/g, '01').length;
+};
+
 const box = (str) => {
-  const rowWhiteLen = 5;
+  const rowWhiteLen = 3;
   const rowWhiteStr = ' '.repeat(rowWhiteLen);
-  const col = rowWhiteLen + str.length + rowWhiteLen;
+  const col = rowWhiteLen + getBLen(str) + rowWhiteLen;
   const boxStr =
     `┌${'─'.repeat(col)}┐\n` +
+    `│${' '.repeat(col)}│\n` +
     `│${rowWhiteStr}${str}${rowWhiteStr}│\n` +
+    `│${' '.repeat(col)}│\n` +
     `└${'─'.repeat(col)}┘`;
 
   console.log('');
